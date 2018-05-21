2fadbb55156bef2d1fc0ae69a49f72f18c723087
cervantes <lmartellguevara@gmail.com>
2018-05-21 17:15:32 -0400

    light-injectable

---
+++ lib/types/index.d.ts
@@ -0,0 +1,3 @@
+import 'reflect-metadata';
+export declare function inject(): (target: any, property?: string) => any;
+export declare function injectable(): (target: any) => any;
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import 'reflect-metadata';
+
+const metadataKeys = {
+  injectable: 'custom:injectable'
+};
+
+export function inject() {
+  return (target: any, property?: string) => {
+    if (property) {
+      const type = Reflect.getOwnMetadata("design:type", target, property);
+      if (type) {
+        const currentInstance = Reflect.getMetadata(metadataKeys.injectable, type);
+        if (currentInstance) {
+          target[property] = currentInstance;
+        }
+      }
+    } else {
+      return spellbook(target);
+    }
+  };
+}
+
+export function injectable() {
+  return (target: any) => {
+    const newConstructor = spellbook(target) || target;
+    Reflect.defineMetadata(metadataKeys.injectable, new newConstructor(), newConstructor);
+    return newConstructor;
+  };
+}
+
+function spellbook(target) {
+  const paramTypes: any[] = Reflect.getOwnMetadata("design:paramtypes", target);
+  if (paramTypes) {
+    const newArgs = paramTypes.map((param) => {
+      const currentInstance = Reflect.getMetadata(metadataKeys.injectable, param);
+      if (currentInstance) {
+        return currentInstance;
+      }
+    });
+    return target.bind(null, ...newArgs);
+  }
+}
